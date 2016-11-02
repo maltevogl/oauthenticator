1a6639da379f614e4c35955bc5f8576c1746daac
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2016-11-02 18:11:40 +0100

    Fix env

--- oauthenticator/google.py
+++ oauthenticator/google.py
@@ -18,7 +18,9 @@ from jupyterhub.utils    import url_path_join
 
 from .oauth2 import OAuthLoginHandler, OAuthCallbackHandler, OAuthenticator
 
+
 class OpenIDOAuth2Mixin(GoogleOAuth2Mixin):
+    GITHUB_HOST = os.environ.get('GITHUB_HOST')
     _OAUTH_AUTHORIZE_URL = "https://%s/authorize" % GITHUB_HOST
     _OAUTH_ACCESS_TOKEN_URL = "https://%s/token" % GITHUB_HOST
 
