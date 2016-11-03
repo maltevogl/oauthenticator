180deee88e06cddc60c9efc731485dad0c86f424
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2016-11-03 13:51:06 +0100

    fix2

--- oauthenticator/google.py
+++ oauthenticator/google.py
@@ -19,7 +19,7 @@ from jupyterhub.utils    import url_path_join
 from .oauth2 import OAuthLoginHandler, OAuthCallbackHandler, OAuthenticator
 
 
-class OpenIDOAuth2Mixin(OAuth2Mixin, OpenIdMixin):
+class OpenIDOAuth2Mixin(OAuth2Mixin):
     GITHUB_HOST = os.environ.get('GITHUB_HOST')
     _OPENID_ENDPOINT = "%s" % GITHUB_HOST
     _OAUTH_AUTHORIZE_URL = "https://%s/authorize" % GITHUB_HOST
@@ -61,8 +61,9 @@ class GoogleOAuthHandler(OAuthCallbackHandler, OpenIDOAuth2Mixin):
 
         # "Cannot redirect after headers have been written" ?
         #OAuthCallbackHandler.get(self)
+        self.log.debug(': "%s"', str(username))
         username = yield self.authenticator.get_authenticated_user(self, None)
-        print(username)
+
         self.log.info('google: username: "%s"', username)
         if username:
             user = self.user_from_username(username)
