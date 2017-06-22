4adceb08748d8b53d54f9287812a9f6b57639677
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-06-22 15:35:13 +0200

    adding tests_1

--- oauthenticator/tests/test_openid.py
+++ oauthenticator/tests/test_openid.py
@@ -6,6 +6,7 @@ from tornado.web import Application, HTTPError
 from ..openid import OpenIDOAuthenticator, OpenIDOAuthHandler
 
 CONNECTOR = github
+_OPENID_ENDPOINT = openid.com
 
 from .mocks import setup_oauth_mock
 
